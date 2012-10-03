e9c501c25676ab32e53b290aef2e34a98053b894
Ryan Alyn Porter <rap@endymion.com>
2012-10-03 19:09:36 -0400

    Using LinkChecker instead of Link::Checker will probably reduce the potential for conflicts with other gems.

--- lib/link_checker.rb
+++ lib/link_checker.rb
@@ -5,69 +5,65 @@ require 'net/https'
 require 'uri'
 require 'colorize'
 
-module Link
+class LinkChecker
 
-  class Checker
+  def initialize(target_path)
+    @target_path = target_path
+  end
 
-    def initialize(target_path)
-      @target_path = target_path
-    end
+  def find_html_files
+    Find.find(@target_path).map {|path|
+      FileTest.file?(path) && (path =~ /\.html?$/) ? path : nil
+    }.reject{|path| path.nil?}
+  end
 
-    def find_html_files
-      Find.find(@target_path).map {|path|
-        FileTest.file?(path) && (path =~ /\.html?$/) ? path : nil
-      }.reject{|path| path.nil?}
-    end
+  def self.find_external_links(file_path)
+    Nokogiri::HTML(open(file_path)).css('a').
+      select{|link| link.attribute('href').value =~ /^https?\:\/\// }
+  end
 
-    def self.find_external_links(file_path)
-      Nokogiri::HTML(open(file_path)).css('a').
-        select{|link| link.attribute('href').value =~ /^https?\:\/\// }
-    end
-
-    def self.check_link(uri)
-      uri = URI.parse(uri)
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = true if uri.scheme == "https"
-      http.start do
-        path = (uri.path.empty?) ? '/' : uri.path
-        http.request_get(path) do |response|
-          case response
-          when Net::HTTPSuccess then
-            return true
-          when Net::HTTPRedirection then
-            return self.check_link(response['location'])
-          else
-            raise Error.new(response)
-          end
-        end
-      end
-    end
-
-    def check_links
-      find_html_files.each do |file|
-        bad_checks = []  
-
-        self.class.find_external_links(file).each do |link|
-          uri = link.attribute('href').value
-          begin
-            self.class.check_link(uri)
-          rescue => error
-            bad_checks << { :link => link, :error => error }
-          end
-        end
-        
-        if bad_checks.empty?
-          puts "Checked: #{file}".green
+  def self.check_link(uri)
+    uri = URI.parse(uri)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true if uri.scheme == "https"
+    http.start do
+      path = (uri.path.empty?) ? '/' : uri.path
+      http.request_get(path) do |response|
+        case response
+        when Net::HTTPSuccess then
+          return true
+        when Net::HTTPRedirection then
+          return self.check_link(response['location'])
         else
-          puts "Problem: #{file}".red
-          bad_checks.each do |check|
-            puts "   Link: #{check[:link].attribute('href').value}".red
-            puts "     Response: #{check[:error].response.inspect}".red
-          end
+          raise Error.new(response)
         end
       end
     end
+  end
 
+  def check_links
+    find_html_files.each do |file|
+      bad_checks = []  
+
+      self.class.find_external_links(file).each do |link|
+        uri = link.attribute('href').value
+        begin
+          self.class.check_link(uri)
+        rescue => error
+          bad_checks << { :link => link, :error => error }
+        end
+      end
+      
+      if bad_checks.empty?
+        puts "Checked: #{file}".green
+      else
+        puts "Problem: #{file}".red
+        bad_checks.each do |check|
+          puts "   Link: #{check[:link].attribute('href').value}".red
+          puts "     Response: #{check[:error].response.inspect}".red
+        end
+      end
+    end
   end
 
   class Error < StandardError
--- spec/link-checker_spec.rb
+++ spec/link-checker_spec.rb
@@ -1,20 +1,19 @@
 require 'spec_helper'
 require 'link_checker'
 
-describe Link::Checker do
+describe LinkChecker do
 
   before(:all) do
     @site_path = 'spec/test-site/public/'
   end
 
   it "finds all of the HTML files in the target path." do
-    checker = Link::Checker.new(@site_path)
-    files = checker.find_html_files
+    files = LinkChecker.new(@site_path).find_html_files
     files.size.should == 3
   end
 
   it "finds all of the external links in an HTML file." do
-    links = Link::Checker.find_external_links(
+    links = LinkChecker.find_external_links(
       'spec/test-site/public/blog/2012/10/02/a-list-of-links/index.html')
     links.size.should == 4
   end
@@ -33,12 +32,12 @@ describe Link::Checker do
     end
 
     it "declares good links to be good." do
-      Link::Checker.check_link(@good_uri).should be true
+      LinkChecker.check_link(@good_uri).should be true
     end
 
     it "declares bad links to be bad." do
-      expect { Link::Checker.check_link(@bad_uri) }.to(
-        raise_error(Link::Error))
+      expect { LinkChecker.check_link(@bad_uri) }.to(
+        raise_error(LinkChecker::Error))
     end
 
     describe "follows redirects to the destination and" do
@@ -46,14 +45,14 @@ describe Link::Checker do
       it "declares good redirect targets to be good." do
         FakeWeb.register_uri(:get, @redirect_uri,
           :location => @good_uri, :status => ["302", "Moved"])
-        Link::Checker.check_link(@redirect_uri).should be true
+        LinkChecker.check_link(@redirect_uri).should be true
       end
 
       it "declares bad redirect targets to be bad." do
         FakeWeb.register_uri(:get, @redirect_uri,
           :location => @bad_uri, :status => ["302", "Moved"])
-        expect { Link::Checker.check_link(@redirect_uri) }.to(
-          raise_error(Link::Error))
+        expect { LinkChecker.check_link(@redirect_uri) }.to(
+          raise_error(LinkChecker::Error))
       end
 
     end
@@ -73,17 +72,17 @@ describe Link::Checker do
     end
 
     it "prints green when the links are all good." do
-      Link::Checker.stub(:check_link) { true }
+      LinkChecker.stub(:check_link) { true }
       $stdout.should_receive(:puts).with(/Checked/i).exactly(3).times
-      Link::Checker.new(@site_path).check_links
+      LinkChecker.new(@site_path).check_links
     end
 
     it "prints green when the links are all bad." do
-      Link::Checker.stub(:check_link).and_raise Link::Error.new('blah')
+      LinkChecker.stub(:check_link).and_raise LinkChecker::Error.new('blah')
       $stdout.should_receive(:puts).with(/Problem/i).exactly(3).times
       $stdout.should_receive(:puts).with(/Link/i).at_least(3).times
       $stdout.should_receive(:puts).with(/Response/i).at_least(3).times
-      Link::Checker.new(@site_path).check_links
+      LinkChecker.new(@site_path).check_links
     end
 
 
