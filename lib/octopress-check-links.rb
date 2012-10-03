dcfb39a1d560a6b5240dc3b01ea988224381c582
Ryan Alyn Porter <rap@endymion.com>
2012-10-02 22:23:59 -0400

    Reorganized from OctopressLinkChecker to Link::Checker.  (More general.)

--- lib/octopress-check-links.rb
+++ lib/octopress-check-links.rb
@@ -2,31 +2,62 @@ require 'find'
 require 'nokogiri'
 require 'net/http'
 
-class OctopressLinkChecker
+module Link
 
-  def self.find_html_files(target_path)
-    html_files = []
-    Find.find(target_path) do |path|
-      if FileTest.directory?(path)
-        if File.basename(path)[0] == '..'
-          Find.prune
+  class Checker
+
+    def initialize(target_path)
+      @target_path = target_path
+    end
+
+    def find_html_files
+      html_files = []
+      Find.find(@target_path) do |path|
+        if FileTest.directory?(path)
+          if File.basename(path)[0] == '..'
+            Find.prune
+          else
+            next
+          end
         else
-          next
+          html_files << path if path =~ /\.html$/
         end
-      else
-        html_files << path if path =~ /\.html$/
+      end
+      html_files
+    end
+
+    def self.find_external_links(file_path)
+      all_links = Nokogiri::HTML(open(file_path)).css('a')
+      external_links = all_links.select{|link| link.attribute('href').value =~ /^http/ }
+    end
+
+    def self.check_link(uri)
+      Net::HTTP.get_response(URI.parse(uri)).class.eql? Net::HTTPOK
+    end
+
+    def check_links
+      find_html_files.each do |file|
+        bad_links = []  
+
+        self.class.find_external_links(file).each do |link|
+          uri = link.attribute('href').value
+          unless self.class.check_link(uri)
+            bad_links << link
+          end
+        end
+        
+        if bad_links.empty?
+          puts "Checked: #{file}".green
+        else
+          puts "Problem: #{file}".red
+          bad_links.each do |link|
+            puts "   Link: #{link.attribute('href').value}".red
+          end
+        end
+
       end
     end
-    html_files
-  end
 
-  def self.find_external_links(target_path)
-    all_links = Nokogiri::HTML(open(target_path)).css('a')
-    external_links = all_links.select{|link| link.attribute('href').value =~ /^http/ }
-  end
-
-  def self.check_link(uri)
-    Net::HTTP.get_response(URI.parse(uri)).class.eql? Net::HTTPOK
   end
 
 end
--- spec/octopress-check-links_spec.rb
+++ spec/octopress-check-links_spec.rb
@@ -1,15 +1,16 @@
 require 'spec_helper'
 require 'octopress-check-links'
 
-describe OctopressLinkChecker, "Link checker" do
+describe Link::Checker, "Link checker" do
 
   it "finds all of the HTML files in the Octopress site" do
-    files = OctopressLinkChecker.find_html_files('spec/test-site/public')
+    checker = Link::Checker.new('spec/test-site/public')
+    files = checker.find_html_files
     files.size.should == 3
   end
 
   it "finds all of the external links in an HTML file" do
-    links = OctopressLinkChecker.find_external_links(
+    links = Link::Checker.find_external_links(
       'spec/test-site/public/blog/2012/10/02/a-list-of-links/index.html')
     links.size.should == 4
   end
@@ -17,12 +18,12 @@ describe OctopressLinkChecker, "Link checker" do
   it "checks links" do
     good_uri = 'http://goodlink.com'
     FakeWeb.register_uri(:any, good_uri, :body => "Yay it worked.")
-    OctopressLinkChecker.check_link(good_uri).should be true
+    Link::Checker.check_link(good_uri).should be true
 
     bad_uri = 'http://brokenlink.com'
     FakeWeb.register_uri(:get, bad_uri,
       :body => "File not found", :status => ["404", "Missing"])
-    OctopressLinkChecker.check_link(bad_uri).should_not be true    
+    Link::Checker.check_link(bad_uri).should_not be true    
   end
 
 end
